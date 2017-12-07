1f53960e1ddabd31ff19cf80848a0bd27fa87d43
Aman Mathur <aman.mathur@upgrad.com>
2017-12-07 11:29:40 +0530

    [AG-546]: Added handler for post_save when objects are created (in order to get ID)

--- event_logger/signals.py
+++ event_logger/signals.py
@@ -1,7 +1,8 @@
 import logging
 
 from django.conf import settings
-from django.db.models.signals import pre_save
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.signals import pre_save, post_save
 from django.dispatch.dispatcher import receiver
 
 from .models import EventLog
@@ -11,7 +12,7 @@ from .helpers import conditions_evaluator
 logger = logging.getLogger(__name__)
 
 
-def additional_info_adder(data, original_object=None):
+def additional_info_adder(data, _object=None):
     for import_statement in settings.EVENT_LOGGER_OUTPUT_FORMAT.get("imports", []):
         exec(import_statement)
     for column_name, column_details in settings.EVENT_LOGGER_OUTPUT_FORMAT.get("columns", {}).items():
@@ -22,10 +23,13 @@ def additional_info_adder(data, original_object=None):
         data[column_name] = column_value
 
 
-def create_signal_receiever(sender, tracked_fields, app_name=None):
+def create_update_signal_receiever(sender, tracked_fields, app_name=None):
     @receiver(pre_save, sender=sender)
     def track_field_changes(sender, instance, **kwargs):
-        old_instance = sender.objects.get(id=instance.id)
+        try:
+            old_instance = sender.objects.get(id=instance.id)
+        except ObjectDoesNotExist:
+            return
         for field_name, field_details in tracked_fields.items():
             to_update = conditions_evaluator(old_instance, field_details.get("conditions"))
             if not to_update:
@@ -43,7 +47,35 @@ def create_signal_receiever(sender, tracked_fields, app_name=None):
                     'app_name': app_name,
                     'object_id': instance.id,
                 }
-                additional_info_adder(data, original_object=old_instance)
+                additional_info_adder(data, _object=old_instance)
+                logger.debug(data)
+                EventLog.objects.create(**data)
+    return track_field_changes
+
+
+def create_init_signal_receiever(sender, tracked_fields, app_name=None):
+    @receiver(post_save, sender=sender)
+    def track_field_changes(sender, instance, created=False, **kwargs):
+        if not created:
+            return
+        for field_name, field_details in tracked_fields.items():
+            to_update = conditions_evaluator(instance, field_details.get("conditions"))
+            if not to_update:
+                continue
+            prev_value = None
+            new_value = getattr(instance, field_name)
+            if prev_value != new_value:
+                field_type = new_value.__class__.__name__
+                data = {
+                    'field_name': field_name,
+                    'before': prev_value,
+                    'after': new_value,
+                    'column_type': field_type,
+                    'model_name': sender.__name__,
+                    'app_name': app_name,
+                    'object_id': instance.id,
+                }
+                additional_info_adder(data, _object=instance)
                 logger.debug(data)
                 EventLog.objects.create(**data)
     return track_field_changes
@@ -52,7 +84,13 @@ def create_signal_receiever(sender, tracked_fields, app_name=None):
 for app_name, model_details in settings.EVENT_LOGGER_TRACKED_FIELDS.items():
     for model_name, field_details in model_details.items():
         sender = '.'.join([app_name, model_name])
-        signal_receiver = create_signal_receiever(sender, field_details, app_name=app_name)
-        func_name = 'signal_receiver_{app_name}_{model_name}'.format(
+
+        init_signal_receiver = create_init_signal_receiever(sender, field_details, app_name=app_name)
+        init_func_name = 'init_signal_receiver_{app_name}_{model_name}'.format(
             app_name=app_name, model_name=model_name)
-        locals()[func_name] = signal_receiver
+        locals()[init_func_name] = init_signal_receiver
+
+        update_signal_receiver = create_update_signal_receiever(sender, field_details, app_name=app_name)
+        update_func_name = 'update_signal_receiver_{app_name}_{model_name}'.format(
+            app_name=app_name, model_name=model_name)
+        locals()[update_func_name] = update_signal_receiver
